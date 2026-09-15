d75c054ee63f3013a7fd3c122aad0af30f215da8
KDakshayani <karimaddeladakshayani@gmail.com>
2026-09-15 11:47:53 +0530

    Lab Program4

---
+++ Lab_program4.py
@@ -0,0 +1,9 @@
+a= float(input("Enter the first number: "))
+b= float(input("Enter the second number: "))
+
+
+add = num1 + num2
+sub= num1 - num2
+
+print(f"The sum of {a} and {b} is: {add}")
+print(f"The difference between {a} and {b} is: {sub}")
